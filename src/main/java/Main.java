d29742db3287c98f7e05f08a4e5c1a304080211b
irinababunova <xleyla@gmail.com>
2023-10-29 03:08:58 +0300

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Variable num = new Variable(6);
+        System.out.println(num.getNewInt());
+        num.setNewInt(10);
+        System.out.println(num.getNewInt());
+    }
+}
---
+++ src/main/java/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    private int newInt;
+
+    public Variable(int i) {
+        this.newInt = i;
+    }
+
+    public int getNewInt() {
+        return newInt;
+    }
+
+    public void setNewInt(int newInt) {
+        this.newInt = newInt;
+    }
+
+}
